5272c5d85749c273ebacac71c969dd61b7a9131c
Sergey Podunov <podunov.sergey@gmail.com>
2014-04-23 00:43:38 +0400

    initial commit

---
+++ src/main/java/ru/hh/hadoop/testing/App.java
@@ -0,0 +1,13 @@
+package ru.hh.hadoop.testing;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
